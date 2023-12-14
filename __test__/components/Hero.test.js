289f0bb30594b79e6cbd8d0f95a727c2e5fb13e7
Andy Ramaroson <andyramaroson@gmail.com>
2023-12-14 13:16:06 +0100

    [setup] initial commit

---
+++ __test__/components/Hero.test.js
@@ -0,0 +1,26 @@
+import React, { createContext }  from "react";
+import { render, screen } from "@testing-library/react";
+import 'intersection-observer'
+import Hero from "@/app/components/Hero";
+
+describe("Hero Component", () => {
+
+  it("should render the title", () => {
+    render(<Hero />);
+    const titleElement = screen.getByText("Creation & développement web par Lemurian Agency");
+    expect(titleElement).toBeInTheDocument();
+  });
+
+
+  it("should render the subtitle", () => {
+    render(<Hero />);
+    const subtitleElement = screen.getByText("Welcome");
+    expect(subtitleElement).toBeInTheDocument();
+  });
+
+  it("should render the hero image", () => {
+    render(<Hero />);
+    const imageElement = screen.getByAltText("hero image");
+    expect(imageElement).toBeInTheDocument();
+  });
+});
